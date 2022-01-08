7b635ae9b7f7ea75c1efb47e1fe4f8ca0521ff6a
danilov_sm <danilov_sm@albank.ru>
2022-01-08 19:08:19 +0900

    lesson 27

--- spring-crud-app1/src/main/java/ru/danilov/springcourse/config/SpringConfig.java
+++ spring-crud-app1/src/main/java/ru/danilov/springcourse/config/SpringConfig.java
@@ -1,10 +1,14 @@
 package ru.danilov.springcourse.config;
 
+import javax.sql.DataSource;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -44,7 +48,24 @@ public class SpringConfig implements WebMvcConfigurer {
     @Override
     public void configureViewResolvers(ViewResolverRegistry registry) {
         ThymeleafViewResolver resolver = new ThymeleafViewResolver();
-        resolver.setTemplateEngine(templateEngine());
+        resolver.setTemplateEngine(templateEngine()); 
         registry.viewResolver(resolver);
+    }
+    
+    @Bean
+    public DataSource dataSource() {
+    	DriverManagerDataSource dataSource = new DriverManagerDataSource();
+    	
+    	dataSource.setDriverClassName("org.postgresql.Driver");
+    	dataSource.setUrl("jdbc:postgresql://localhost:5432/postgres");
+    	dataSource.setUsername("postgres");
+    	dataSource.setPassword("Qwerty123!");
+    	
+    	return dataSource;
+    }
+    
+    @Bean
+    public JdbcTemplate jdbcTemplate() {
+    	return new JdbcTemplate(dataSource());
     }
 }
---
+++ spring-crud-app1/src/main/java/ru/danilov/springcourse/dao/PersonMapper.java
@@ -0,0 +1,25 @@
+package ru.danilov.springcourse.dao;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import org.springframework.jdbc.core.RowMapper;
+
+import ru.danilov.springcourse.models.Person;
+
+public class PersonMapper implements RowMapper<Person> {
+
+	@Override
+	public Person mapRow(ResultSet resultSet, int i) throws SQLException {
+		
+		Person person = new Person();
+		
+		person.setId(resultSet.getInt("id"));
+		person.setName(resultSet.getString("name"));
+		person.setAge(resultSet.getInt("age"));
+		person.setEmail(resultSet.getString("email"));
+		
+		return person;
+	}
+
+}
